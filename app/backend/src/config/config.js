f0f16df51f10d27ce30a6fedc1e65681a7a61754
Alec Medeiros <alecmm@outlook.com>
2022-12-19 14:45:17 -0300

    Docking the App

---
+++ app/backend/src/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+};
